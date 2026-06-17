87ca588fd2964eb1bb032a3ac12604d7bbdb0bc8
FlamurMaliqi <flamurmaliqi@MacBook-Pro-von-FlamurMaliqi.local>
2026-06-17 13:23:59 +0200

    VAY-848 clean up Cloudflare DNS output

--- infra/cloudflare.tf
+++ infra/cloudflare.tf
@@ -2,6 +2,6 @@ resource "cloudflare_record" "target_api" {
   zone_id = var.cloudflare_zone_id
   name    = "target-api"
   type    = "CNAME"
-  value   = data.aws_lb.main.dns_name
+  content = data.aws_lb.main.dns_name
   proxied = false
 }
