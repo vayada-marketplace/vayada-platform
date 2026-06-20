dd8192809d061f0ae9c2d587c2d56d1cecccf097
FlamurMaliqi <flamurmaliqi@MacBook-Pro-von-FlamurMaliqi.local>
2026-06-20 15:38:03 +0200

    VAY-862 add next booking wildcard TLS

--- infra/acm.tf
+++ infra/acm.tf
@@ -72,7 +72,44 @@ resource "aws_acm_certificate_validation" "wildcard_booking" {
   validation_record_fqdns = [for record in aws_route53_record.wildcard_booking_validation : record.fqdn]
 }
 
-# Attach both certs to the existing ALB HTTPS listener
+# Wildcard cert for *.next-booking.vayada.com
+# Covers: <hotel-slug>.next-booking.vayada.com rehearsal tenant subdomains
+resource "aws_acm_certificate" "wildcard_next_booking" {
+  domain_name       = "*.next-booking.vayada.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "wildcard-next-booking-vayada-com"
+  }
+}
+
+resource "aws_route53_record" "wildcard_next_booking_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.wildcard_next_booking.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main.zone_id
+}
+
+resource "aws_acm_certificate_validation" "wildcard_next_booking" {
+  certificate_arn         = aws_acm_certificate.wildcard_next_booking.arn
+  validation_record_fqdns = [for record in aws_route53_record.wildcard_next_booking_validation : record.fqdn]
+}
+
+# Attach wildcard certs to the existing ALB HTTPS listener
 resource "aws_lb_listener_certificate" "wildcard_vayada" {
   listener_arn    = data.aws_lb_listener.https.arn
   certificate_arn = aws_acm_certificate_validation.wildcard_vayada.certificate_arn
@@ -81,4 +118,9 @@ resource "aws_lb_listener_certificate" "wildcard_vayada" {
 resource "aws_lb_listener_certificate" "wildcard_booking" {
   listener_arn    = data.aws_lb_listener.https.arn
   certificate_arn = aws_acm_certificate_validation.wildcard_booking.certificate_arn
+}
+
+resource "aws_lb_listener_certificate" "wildcard_next_booking" {
+  listener_arn    = data.aws_lb_listener.https.arn
+  certificate_arn = aws_acm_certificate_validation.wildcard_next_booking.certificate_arn
 }
